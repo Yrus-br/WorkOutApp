10e92fea4daf8d1efff15bfcb301d058056c06da
Yurii <yourussianbrother@gmail.com>
2022-11-13 01:26:02 +0300

    WeekView configuration

--- WorkOutApp/Controllers/Overview/Views/weekView/WeekView.swift
+++ WorkOutApp/Controllers/Overview/Views/weekView/WeekView.swift
@@ -39,9 +39,9 @@ extension WeekView {
         
         let weekdays = calendar.shortStandaloneWeekdaySymbols
         
-        weekdays.forEach { _ in
+        weekdays.enumerated().forEach { index, name in
             let view = WeekdayView()
-            
+            view.configure(with: index, and: name)
             stackView.addArrangedSubview(view)
         }
     }
--- WorkOutApp/Controllers/Overview/Views/weekView/WeekdayView.swift
+++ WorkOutApp/Controllers/Overview/Views/weekView/WeekdayView.swift
@@ -13,6 +13,11 @@ extension WeekView {
         private let nameLabel = UILabel()
         private let dateLabel = UILabel()
         private let stackView = UIStackView()
+        
+        func configure(with index: Int, and name: String) {
+            nameLabel.text = name
+            dateLabel.text = "\(index)"
+        }
     }
 }
 
@@ -30,14 +35,22 @@ extension WeekView.WeekdayView {
         
         NSLayoutConstraint.activate([
             stackView.centerXAnchor.constraint(equalTo: centerXAnchor),
-            stackView.centerYAnchor.constraint(equalTo: centerYAnchor)
+            stackView.centerYAnchor.constraint(equalTo: centerYAnchor),
         ])
     }
     
     override func configureViews() {
         super.configureViews()
         
-        backgroundColor = .red
+        layer.cornerRadius = 5
+        layer.masksToBounds = true
+        
+        nameLabel.font = Resources.Fonts.helveticaRegular(with: 9)
+        nameLabel.textAlignment = .center
+        
+        dateLabel.font = Resources.Fonts.helveticaRegular(with: 15)
+        nameLabel.textAlignment = .center
+
         
         stackView.spacing = 3
         stackView.axis = .vertical 
